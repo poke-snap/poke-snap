d6c6e0a80b2be4214c70e25f21a36c6c92a10507
Sean Takafuji <seanytak@hawaii.edu>
2019-02-06 16:28:17 -1000

    Expo Camera takes pictures

--- screens/CameraScreen.js
+++ screens/CameraScreen.js
@@ -7,62 +7,91 @@ import {
   Text,
   TouchableOpacity,
   View,
-} from 'react-native';
-import { WebBrowser } from 'expo';
 
-import { MonoText } from '../components/StyledText';
+} from 'react-native';
+import { Camera, Permissions, WebBrowser } from 'expo';
 
 export default class CameraScreen extends React.Component {
+
+  state = {
+    hasCameraPermission: null,
+    type: Camera.Constants.Type.back,
+  };
+
+  async componentDidMount() {
+    const { status } = await Permissions.askAsync(Permissions.CAMERA);
+    this.setState({ hasCameraPermission: status === 'granted' });
+  }
+
   static navigationOptions = {
     header: null,
   };
 
+  takePicture = () => {
+    console.log('Taking Picture');
+    if (this.camera) {
+      console.log('Took picture');
+      this.camera.takePictureAsync({ onPictureSaved: this.onPictureSaved });
+    }
+  };
+
   render() {
-    return (
-      <View style={styles.container}>
-        <ScrollView style={styles.container} contentContainerStyle={styles.contentContainer}>
-          <View style={styles.welcomeContainer}>
-            <Image
-              source={
-                __DEV__
-                  ? require('../assets/images/robot-dev.png')
-                  : require('../assets/images/robot-prod.png')
-              }
-              style={styles.welcomeImage}
-            />
-          </View>
+    console.log('Rendering Camera');
+    const { hasCameraPermission } = this.state;
+    if (hasCameraPermission === null) {
+      return <View />;
+    } else if (hasCameraPermission === false) {
+      return <Text>No access to camera</Text>;
+    } else {
+      return (
+        <View style={{ flex: 1 }}>
+          <Camera 
+            style={{ flex: 1 }} 
+            type={this.state.type} 
+            ref={ref => {
+            this.camera = ref;
+          }}>
+            <View
+              style={{
+                flex: 1,
+                backgroundColor: 'transparent',
+                flexDirection: 'row',
+              }}>
+              <TouchableOpacity
+                style={{
+                  flex: 0.1,
+                  alignSelf: 'flex-end',
+                  alignItems: 'center',
+                }}
 
-          <View style={styles.getStartedContainer}>
-            {this._maybeRenderDevelopmentModeWarning()}
-
-            <Text style={styles.getStartedText}>Get started by opening</Text>
-            <Text>HI</Text>
-
-            <View style={[styles.codeHighlightContainer, styles.homeScreenFilename]}>
-              <MonoText style={styles.codeHighlightText}>screens/HomeScreen.js</MonoText>
+                // onPress={() => {
+                //   this.setState({
+                //     type: this.state.type === Camera.Constants.Type.back
+                //       ? Camera.Constants.Type.front
+                //       : Camera.Constants.Type.back,
+                //   });
+                // }}>
+                onPress={this.takePicture}>
+                <Text
+                  style={{ fontSize: 18, marginBottom: 10, color: 'white' }}>
+                  {' '}Flip{' '}
+                </Text>
+              </TouchableOpacity>
             </View>
-
-            <Text style={styles.getStartedText}>
-              Change this text and your app will automatically reload.
-            </Text>
-          </View>
-
-          <View style={styles.helpContainer}>
-            <TouchableOpacity onPress={this._handleHelpPress} style={styles.helpLink}>
-              <Text style={styles.helpLinkText}>Help, it didn’t automatically reload!</Text>
-            </TouchableOpacity>
-          </View>
-        </ScrollView>
-
-        <View style={styles.tabBarInfoContainer}>
-          <Text style={styles.tabBarInfoText}>This is a tab bar. You can edit it in:</Text>
-
-          <View style={[styles.codeHighlightContainer, styles.navigationFilename]}>
-            <MonoText style={styles.codeHighlightText}>navigation/MainTabNavigator.js</MonoText>
-          </View>
+            <View>
+              <TouchableOpacity
+                onPress={this.takePicture}
+                style={{ alignSelf: 'center' }}
+              ></TouchableOpacity>
+              <Text
+                style={{ fontSize: 18, marginBottom: 10, color: 'white' }}>
+                {' '}Flip{' '}
+              </Text>
+            </View>
+          </Camera>
         </View>
-      </View>
-    );
+      );
+    }
   }
 
   _maybeRenderDevelopmentModeWarning() {
