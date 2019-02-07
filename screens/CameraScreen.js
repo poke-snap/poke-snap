c5969e0c83f0f8d04383e6ca143866b56455427d
Sean Takafuji <seanytak@hawaii.edu>
2019-02-06 18:51:14 -1000

    Added camera configurations

--- screens/CameraScreen.js
+++ screens/CameraScreen.js
@@ -9,49 +9,190 @@ import {
   View,
 
 } from 'react-native';
-import { Camera, Permissions, WebBrowser } from 'expo';
+import { Camera, Constants, FileSystem, Permissions, WebBrowser } from 'expo';
+import {
+  Ionicons,
+  MaterialIcons,
+  Foundation,
+  MaterialCommunityIcons,
+  Octicons
+} from '@expo/vector-icons';
 
+const landmarkSize = 2;
+
+const flashModeOrder = {
+  off: 'on',
+  on: 'auto',
+  auto: 'torch',
+  torch: 'off',
+};
+
+const flashIcons = {
+  off: 'flash-off',
+  on: 'flash-on',
+  auto: 'flash-auto',
+  torch: 'highlight'
+};
+
+const wbOrder = {
+  auto: 'sunny',
+  sunny: 'cloudy',
+  cloudy: 'shadow',
+  shadow: 'fluorescent',
+  fluorescent: 'incandescent',
+  incandescent: 'auto',
+};
+
+const wbIcons = {
+  auto: 'wb-auto',
+  sunny: 'wb-sunny',
+  cloudy: 'wb-cloudy',
+  shadow: 'beach-access',
+  fluorescent: 'wb-iridescent',
+  incandescent: 'wb-incandescent',
+};
 export default class CameraScreen extends React.Component {
-
   state = {
-    hasCameraPermission: null,
-    type: Camera.Constants.Type.back,
+    flash: 'off',
+    zoom: 0,
+    autoFocus: 'on',
+    type: 'back',
+    whiteBalance: 'auto',
+    ratio: '16:9',
+    ratios: [],
+    barcodeScanning: false,
+    faceDetecting: false,
+    faces: [],
+    newPhotos: false,
+    permissionsGranted: false,
+    pictureSize: undefined,
+    pictureSizes: [],
+    pictureSizeId: 0,
+    showGallery: false,
+    showMoreOptions: false,
   };
 
   async componentDidMount() {
     const { status } = await Permissions.askAsync(Permissions.CAMERA);
-    this.setState({ hasCameraPermission: status === 'granted' });
+    this.setState({ permissionGranted: status === 'granted' });
+    FileSystem.makeDirectoryAsync(FileSystem.documentDirectory + 'photos').catch(e => {
+      console.log(e, 'Directory exists');
+    });
   }
 
   static navigationOptions = {
     header: null,
   };
 
-  takePicture = () => {
-    console.log('Taking Picture');
+  onPictureSaved = async photo => {
+    // console.log('')
+    await FileSystem.moveAsync({
+      from: photo.uri,
+      to: `${FileSystem.documentDirectory}photos/${Date.now()}.jpg`,
+    });
+    this.setState({ newPhotos: true });
+  }
+
+  takePicture = async () => {
+    console.log('[Function Call] takePicture');
     if (this.camera) {
-      console.log('Took picture');
+      console.log('[takePicture] Took Picture');
       this.camera.takePictureAsync({ onPictureSaved: this.onPictureSaved });
     }
   };
 
+  toggleFacing = () => this.setState({ type: this.state.type === 'back' ? 'front' : 'back' });
+
+  toggleFlash = () => this.setState({ flash: flashModeOrder[this.state.flash] });
+
+  toggleFocus = () => this.setState({ autoFocus: this.state.autoFocus === 'on' ? 'off' : 'on' });
+
+  toggleView = () => this.setState({ showGallery: !this.state.showGallery, newPhotos: false });
+
+  renderTopBar = () => {
+    return (
+      <View
+        style={styles.topBar}>
+        <TouchableOpacity style={styles.toggleButton} onPress={this.toggleFacing}>
+          <Ionicons name="ios-reverse-camera" size={32} color="white" />
+        </TouchableOpacity>
+        <TouchableOpacity style={styles.toggleButton} onPress={this.toggleFlash}>
+          <MaterialIcons name={flashIcons[this.state.flash]} size={32} color="white" />
+        </TouchableOpacity>
+        <TouchableOpacity style={styles.toggleButton} onPress={this.toggleWB}>
+          <MaterialIcons name={wbIcons[this.state.whiteBalance]} size={32} color="white" />
+        </TouchableOpacity>
+        <TouchableOpacity style={styles.toggleButton} onPress={this.toggleFocus}>
+          <Text style={[styles.autoFocusLabel, { color: this.state.autoFocus === 'on' ? "white" : "#6b6b6b" }]}>AF</Text>
+        </TouchableOpacity>
+      </View>
+    );
+  }
+
+  renderBottomBar = () => {
+    return (
+      <View
+        style={styles.bottomBar}>
+        <TouchableOpacity style={styles.bottomButton} onPress={this.toggleMoreOptions}>
+          <Octicons name="kebab-horizontal" size={30} color="white" />
+        </TouchableOpacity>
+        <View style={{ flex: 0.4 }}>
+          <TouchableOpacity style={{ alignSelf: 'center' }} onPress={this.takePicture}>
+            <Ionicons name="ios-radio-button-on" size={70} color="white" />
+          </TouchableOpacity>
+        </View>
+        <TouchableOpacity style={styles.bottomButton} onPress={this.toggleView}>
+          <View>
+            <Foundation name="thumbnails" size={30} color="white" />
+            {this.state.newPhotos && <View style={styles.newPhotosDot} />}
+          </View>
+        </TouchableOpacity>
+      </View>
+    );
+  }
+
+
   render() {
-    console.log('Rendering Camera');
-    const { hasCameraPermission } = this.state;
-    if (hasCameraPermission === null) {
+    // console.log('Rendering Camera');
+    const { permissionGranted } = this.state;
+
+    // Camera 
+    if (permissionGranted === null) {
       return <View />;
-    } else if (hasCameraPermission === false) {
+    }
+    else if (!permissionGranted) {
       return <Text>No access to camera</Text>;
-    } else {
+    }
+    else {
       return (
         <View style={{ flex: 1 }}>
-          <Camera 
-            style={{ flex: 1 }} 
-            type={this.state.type} 
+          <Camera
             ref={ref => {
-            this.camera = ref;
-          }}>
-            <View
+              this.camera = ref;
+            }}
+            style={styles.camera}
+            onCameraReady={this.collectPictureSizes}
+            type={this.state.type}
+            flashMode={this.state.flash}
+            autoFocus={this.state.autoFocus}
+            zoom={this.state.zoom}
+            whiteBalance={this.state.whiteBalance}
+            ratio={this.state.ratio}
+            pictureSize={this.state.pictureSize}
+            onMountError={this.handleMountError}
+            onFacesDetected={this.state.faceDetecting ? this.onFacesDetected : undefined}
+            onFaceDetectionError={this.onFaceDetectionError}
+            // barCodeScannerSettings={{
+            //   barCodeTypes: [
+            //     BarCodeScanner.Constants.BarCodeType.qr,
+            //     BarCodeScanner.Constants.BarCodeType.pdf417,
+            //   ],
+            // }}
+            // onBarCodeScanned={this.state.barcodeScanning ? this.onBarCodeScanned : undefined}
+          >
+            {this.renderTopBar()}
+            {this.renderBottomBar()}
+            {/* <View
               style={{
                 flex: 1,
                 backgroundColor: 'transparent',
@@ -87,132 +228,233 @@ export default class CameraScreen extends React.Component {
                 style={{ fontSize: 18, marginBottom: 10, color: 'white' }}>
                 {' '}Flip{' '}
               </Text>
-            </View>
+            </View> */}
           </Camera>
         </View>
       );
     }
   }
-
-  _maybeRenderDevelopmentModeWarning() {
-    if (__DEV__) {
-      const learnMoreButton = (
-        <Text onPress={this._handleLearnMorePress} style={styles.helpLinkText}>
-          Learn more
-        </Text>
-      );
-
-      return (
-        <Text style={styles.developmentModeText}>
-          Development mode is enabled, your app will be slower but you can use useful development
-          tools. {learnMoreButton}
-        </Text>
-      );
-    } else {
-      return (
-        <Text style={styles.developmentModeText}>
-          You are not in development mode, your app will run at full speed.
-        </Text>
-      );
-    }
-  }
-
-  _handleLearnMorePress = () => {
-    WebBrowser.openBrowserAsync('https://docs.expo.io/versions/latest/guides/development-mode');
-  };
-
-  _handleHelpPress = () => {
-    WebBrowser.openBrowserAsync(
-      'https://docs.expo.io/versions/latest/guides/up-and-running.html#can-t-see-your-changes'
-    );
-  };
 }
+
+// const styles = StyleSheet.create({
+//   container: {
+//     flex: 1,
+//     backgroundColor: '#fff',
+//   },
+//   developmentModeText: {
+//     marginBottom: 20,
+//     color: 'rgba(0,0,0,0.4)',
+//     fontSize: 14,
+//     lineHeight: 19,
+//     textAlign: 'center',
+//   },
+//   contentContainer: {
+//     paddingTop: 30,
+//   },
+//   welcomeContainer: {
+//     alignItems: 'center',
+//     marginTop: 10,
+//     marginBottom: 20,
+//   },
+//   welcomeImage: {
+//     width: 100,
+//     height: 80,
+//     resizeMode: 'contain',
+//     marginTop: 3,
+//     marginLeft: -10,
+//   },
+//   getStartedContainer: {
+//     alignItems: 'center',
+//     marginHorizontal: 50,
+//   },
+//   homeScreenFilename: {
+//     marginVertical: 7,
+//   },
+//   codeHighlightText: {
+//     color: 'rgba(96,100,109, 0.8)',
+//   },
+//   codeHighlightContainer: {
+//     backgroundColor: 'rgba(0,0,0,0.05)',
+//     borderRadius: 3,
+//     paddingHorizontal: 4,
+//   },
+//   getStartedText: {
+//     fontSize: 17,
+//     color: 'rgba(96,100,109, 1)',
+//     lineHeight: 24,
+//     textAlign: 'center',
+//   },
+//   tabBarInfoContainer: {
+//     position: 'absolute',
+//     bottom: 0,
+//     left: 0,
+//     right: 0,
+//     ...Platform.select({
+//       ios: {
+//         shadowColor: 'black',
+//         shadowOffset: { height: -3 },
+//         shadowOpacity: 0.1,
+//         shadowRadius: 3,
+//       },
+//       android: {
+//         elevation: 20,
+//       },
+//     }),
+//     alignItems: 'center',
+//     backgroundColor: '#fbfbfb',
+//     paddingVertical: 20,
+//   },
+//   tabBarInfoText: {
+//     fontSize: 17,
+//     color: 'rgba(96,100,109, 1)',
+//     textAlign: 'center',
+//   },
+//   navigationFilename: {
+//     marginTop: 5,
+//   },
+//   helpContainer: {
+//     marginTop: 15,
+//     alignItems: 'center',
+//   },
+//   helpLink: {
+//     paddingVertical: 15,
+//   },
+//   helpLinkText: {
+//     fontSize: 14,
+//     color: '#2e78b7',
+//   },
+// });
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#000',
   },
-  developmentModeText: {
-    marginBottom: 20,
-    color: 'rgba(0,0,0,0.4)',
-    fontSize: 14,
-    lineHeight: 19,
-    textAlign: 'center',
+  camera: {
+    flex: 1,
+    justifyContent: 'space-between',
   },
-  contentContainer: {
-    paddingTop: 30,
+  topBar: {
+    flex: 0.2,
+    backgroundColor: 'transparent',
+    flexDirection: 'row',
+    justifyContent: 'space-around',
+    paddingTop: Constants.statusBarHeight / 2,
   },
-  welcomeContainer: {
+  bottomBar: {
+    paddingBottom: 5,
+    backgroundColor: 'transparent',
+    alignSelf: 'flex-end',
+    justifyContent: 'space-between',
+    flex: 0.12,
+    flexDirection: 'row',
+  },
+  noPermissions: {
+    flex: 1,
     alignItems: 'center',
-    marginTop: 10,
-    marginBottom: 20,
+    justifyContent: 'center',
+    padding: 10,
   },
-  welcomeImage: {
-    width: 100,
-    height: 80,
-    resizeMode: 'contain',
-    marginTop: 3,
-    marginLeft: -10,
+  gallery: {
+    flex: 1,
+    flexDirection: 'row',
+    flexWrap: 'wrap',
   },
-  getStartedContainer: {
+  toggleButton: {
+    flex: 0.25,
+    height: 40,
+    marginHorizontal: 2,
+    marginBottom: 10,
+    marginTop: 20,
+    padding: 5,
     alignItems: 'center',
-    marginHorizontal: 50,
+    justifyContent: 'center',
   },
-  homeScreenFilename: {
-    marginVertical: 7,
+  autoFocusLabel: {
+    fontSize: 20,
+    fontWeight: 'bold'
   },
-  codeHighlightText: {
-    color: 'rgba(96,100,109, 0.8)',
+  bottomButton: {
+    flex: 0.3,
+    height: 58,
+    justifyContent: 'center',
+    alignItems: 'center',
   },
-  codeHighlightContainer: {
-    backgroundColor: 'rgba(0,0,0,0.05)',
-    borderRadius: 3,
-    paddingHorizontal: 4,
+  newPhotosDot: {
+    position: 'absolute',
+    top: 0,
+    right: -5,
+    width: 8,
+    height: 8,
+    borderRadius: 4,
+    backgroundColor: '#4630EB'
   },
-  getStartedText: {
-    fontSize: 17,
-    color: 'rgba(96,100,109, 1)',
-    lineHeight: 24,
-    textAlign: 'center',
+  options: {
+    position: 'absolute',
+    bottom: 80,
+    left: 30,
+    width: 200,
+    height: 160,
+    backgroundColor: '#000000BA',
+    borderRadius: 4,
+    padding: 10,
   },
-  tabBarInfoContainer: {
+  detectors: {
+    flex: 0.5,
+    justifyContent: 'space-around',
+    alignItems: 'center',
+    flexDirection: 'row',
+  },
+  pictureQualityLabel: {
+    fontSize: 10,
+    marginVertical: 3,
+    color: 'white'
+  },
+  pictureSizeContainer: {
+    flex: 0.5,
+    alignItems: 'center',
+    paddingTop: 10,
+  },
+  pictureSizeChooser: {
+    alignItems: 'center',
+    justifyContent: 'space-between',
+    flexDirection: 'row'
+  },
+  pictureSizeLabel: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center'
+  },
+  facesContainer: {
     position: 'absolute',
     bottom: 0,
-    left: 0,
     right: 0,
-    ...Platform.select({
-      ios: {
-        shadowColor: 'black',
-        shadowOffset: { height: -3 },
-        shadowOpacity: 0.1,
-        shadowRadius: 3,
-      },
-      android: {
-        elevation: 20,
-      },
-    }),
-    alignItems: 'center',
-    backgroundColor: '#fbfbfb',
-    paddingVertical: 20,
+    left: 0,
+    top: 0,
   },
-  tabBarInfoText: {
-    fontSize: 17,
-    color: 'rgba(96,100,109, 1)',
+  face: {
+    padding: 10,
+    borderWidth: 2,
+    borderRadius: 2,
+    position: 'absolute',
+    borderColor: '#FFD700',
+    justifyContent: 'center',
+    backgroundColor: 'rgba(0, 0, 0, 0.5)',
+  },
+  landmark: {
+    width: landmarkSize,
+    height: landmarkSize,
+    position: 'absolute',
+    backgroundColor: 'red',
+  },
+  faceText: {
+    color: '#FFD700',
+    fontWeight: 'bold',
     textAlign: 'center',
+    margin: 10,
+    backgroundColor: 'transparent',
   },
-  navigationFilename: {
-    marginTop: 5,
-  },
-  helpContainer: {
-    marginTop: 15,
-    alignItems: 'center',
-  },
-  helpLink: {
-    paddingVertical: 15,
-  },
-  helpLinkText: {
-    fontSize: 14,
-    color: '#2e78b7',
+  row: {
+    flexDirection: 'row',
   },
 });
